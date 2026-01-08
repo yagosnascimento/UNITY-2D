035d1ec696bfaed0899863e0f37ffdb3fca1ffe7
iKripto <yagosiqueira450@gmail.com>
2026-01-08 14:15:57 -0300

    Inimigo adicionado

---
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+
+
+[RequireComponent(typeof(Animator))]
+
+public abstract class BaseEnemy : MonoBehaviour
+
+{
+
+    protected Animator animator;
+
+
+
+    protected virtual void Awake()
+
+    {
+
+        animator = GetComponent<Animator>();
+
+    }
+
+
+
+    protected abstract void Update();
+
+}
---
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+
+
+public class MeleeEnemy : BaseEnemy
+
+{
+
+    [SerializeField] private Transform detectPosition;
+
+    [SerializeField] private Vector2 detectBoxSize;
+
+    [SerializeField] private LayerMask playerLayer;
+
+
+
+    protected override void Update()
+
+    {
+
+
+        print("is in sight? " + PlayerInSight());
+
+
+        VerifyCanAttack();
+
+
+    }
+
+
+
+
+
+    private void VerifyCanAttack()
+
+
+    {
+
+
+        if (PlayerInSight())
+
+
+        {
+
+
+            animator.SetTrigger("attack");
+
+
+        }
+
+    }
+
+
+
+    private bool PlayerInSight()
+
+    {
+
+        Collider2D playerCollider = Physics2D.OverlapBox(detectPosition.position, detectBoxSize, 0f, playerLayer);
+
+        return playerCollider != null;
+
+    }
+
+
+
+    private void OnDrawGizmos()
+
+    {
+
+        if (detectPosition == null) return;
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireCube(detectPosition.position, detectBoxSize);
+
+    }
+
+}
